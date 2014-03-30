e33db18af3971fec4c0461f8374cd52b8c381c4b
TURLA ZJAN <zjancarlo@gmail.com>
2014-03-30 13:31:19 +0800

    + Successfully creates wrongly rotated (by -90) average patches

--- cs180functions.py
+++ cs180functions.py
@@ -1,8 +1,34 @@
 import math
+import csv
 import pylab as pl
 from matplotlib.pylab import *
 import cPickle as pickle
 from sklearn.feature_extraction import image
+
+def importFeatureArray():
+	with open('training.csv', 'rb') as csvfile:
+		preprocessReader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+		counter = 1
+		testCounter = 5
+		headerTitle = []
+		featureArray = []
+		for row in preprocessReader:
+			#row[30] is image data
+			#print row[30]
+
+			if counter == 1: #row being read is header
+				for header in row:
+					featureArray.append([header])
+				counter -= 1
+			else:	#row being read is data
+				index = 0
+				for item in row:
+					if (item != "") & (item != " "):
+						featureArray[index].append(item)
+					else:
+						featureArray[index].append(-1)	#does not exist for this index
+					index += 1
+		return featureArray
 
 def getCoor(x, y, patch_size = 21, x_total = 96, y_total = 96):
 		x1 = math.floor(x - patch_size/2)
@@ -26,32 +52,56 @@ def getAveragePatch(featureArray, featureNumber, imageGraph):
 	patchArray = []
 
 	#gets patches of all images
+	averagePatch = zeros((21, 21))
+	counter = 0
+
 	for index in range(0, len(imageGraph)):
-		print "Getting image patch at index " + str(index)
-		temp = getPatch(float(featureArray[featureNumber][1 + index]), float(featureArray[featureNumber+1][1 + index]), imageGraph[index])
+		if (index >= 1000) and (index % 1000 == 0):
+			print "Getting image patch at index " + str(index)
+		if(featureArray[featureNumber][1 + index] != -1) and (getCoor(float(featureArray[featureNumber][1 + index]), float(featureArray[featureNumber + 1][1 + index])) < 5776):
+			temp = getPatch(float(featureArray[featureNumber][1 + index]), float(featureArray[featureNumber+1][1 + index]), imageGraph[index])
+		else:
+			temp = [-1]
 		if len(temp) != 1:
-			patchArray.append(temp)
+			counter += 1
+			temp_x = 0
+			temp_y = 0
+			for row in temp:
+				for column in row:
+					averagePatch[temp_x][temp_y] += column
+					temp_x += 1
+				temp_y += 1
+				temp_x = 0
+			temp_y = 0
+			#patchArray.append(temp)
 		else:
 			print "Skipped image patch at index " + str(index)
+	'''
+	for row in averagePatch:
+		for column in row:
+			averagePatch[temp_x][temp_y] = (averagePatch[temp_x][temp_y] / counter)
+			temp_x += 1
+		temp_y += 1
+		temp_x = 0
+	temp_y = 0
+	'''
 
-	averagePatch = zeros((96, 96))
-
+	'''
 	#Gets actual average patch
 	temp_x = 0
 	temp_y = 0
-	counter = 0
 	for image in patchArray:
+		#print "Entering image " + str(counter)
 		for row in image:
-			print "Entering row " + str(counter)
 			for column in row:
 				averagePatch[temp_x][temp_y] += column / len(patchArray)
 				temp_x += 1
 			temp_y += 1
-		temp_x = 0
+			temp_x = 0
 		temp_y = 0
-		count += 1
+	'''
 
-	print averagePatch
+	#print averagePatch
 	return averagePatch
 
 	def getBestMatch(imagePatches, averagePatches, featureNumber):
@@ -67,4 +117,20 @@ def getAveragePatch(featureArray, featureNumber, imageGraph):
 	get averagePatch for features
 	pickle averagePatch
 
+	'''
+
+	'''
+	rotate averagePatch result by -90
+	>>> from scipy import ndimage
+	>>> rotate_lena = ndimage.rotate(lena, 45)
+	http://www.tp.umu.se/~nylen/pylect/advanced/image_processing/index.html
+	'''
+
+	'''
+	to generate all average patches
+	averagePatches = []
+	for index in range(0, 30, 2): #(even indexes are x, odd are y)
+		temp = getAveragePatch(featureArray, index, imageGraph)
+		temp = ndimage.rotate(temp, -90)	#rotates image to proper orientation
+		averagePatch.append(temp)
 	'''
